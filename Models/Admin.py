4267d8592c39e91aa2867f3d9d591f3e4c26e0b3
Paul Kipkemboi <you@example.com>
2026-09-12 12:04:26 +0300

    admin role updated

---
+++ Models/Admin.py
@@ -0,0 +1,28 @@
+import hashlib
+
+
+class Admin:
+    def __init__(self, name, email, password_hash, role="admin"):
+        self.name = name
+        self.email = email
+        self.__password_hash = password_hash
+        self.role = role
+
+    @staticmethod
+    def hash_password(password):
+        return hashlib.sha256(password.encode()).hexdigest()
+
+    def check_password(self, password):
+        return self.__password_hash == self.hash_password(password)
+
+    @property
+    def password_hash(self):
+        return self.__password_hash
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "email": self.email,
+            "password_hash": self.__password_hash,
+            "role": self.role,
+        }
