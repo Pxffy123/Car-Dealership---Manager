458f9bd7bc800ad3dbb172307763806b55179e69
Your Name <you@example.com>
2026-09-13 16:56:10 +0300

    hash password user

--- Models/User.py
+++ Models/User.py
@@ -2,15 +2,14 @@ import hashlib
 
 
 class User:
-    def __init__(self, username, password):
+    
+    def __init__(self, username, password, email=None, role="user"):
         self.username = username
-        self.password = self._hash_password(password)
+        self.password_hash = self._hash_password(password)
+        self.email = email
+        self.role = role
 
     def _hash_password(self, password):
         return hashlib.sha256(password.encode()).hexdigest()   
 
-    def login(self):
-        pass
-
-    def __str__(self):
-        return self.username
+     
