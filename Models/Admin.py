909a0a142d4f39893592f357e1517faded57b3cf
Paul Kipkemboi <you@example.com>
2026-09-13 15:14:30 +0300

    Admin Updated

--- Models/Admin.py
+++ Models/Admin.py
@@ -1,30 +1,4 @@
-import hashlib
-
-class User:
-    def __init__(self, username, password, role="user"):
-        self.username = username
-        
-        self.password_hash = self.hash_password(password)
-        self.role = role
-
-    @staticmethod
-    def hash_password(password):
-        return hashlib.sha256(password.encode()).hexdigest()
-
-    def check_password(self, password):
-        return self.password_hash == self.hash_password(password)
-
-    def to_dict(self):
-        return {
-            "username": self.username,
-            "password_hash": self.password_hash,
-            "role": self.role
-        }
-
-    def __str__(self):
-        return self.username
-
-
+from Models.User import User
 
 class Admin(User):
     def __init__(self, username, password, email):
